962eed985ddc8f8dc557cf3bbaa6c4f29da9563c
nf-mugicha <natsuki.fujimoto.kk@gmail.com>
2018-12-28 04:59:03 +0900

    remove youtube

--- youtube2mp3.py
+++ youtube2mp3.py
@@ -4,6 +4,7 @@
 from pytube import YouTube
 from analize_logging import logger
 import os
+from pydub import AudioSegment
 
 def youtube2mp3(youtube_url, line_userid):
     logger.info('youtube url: {}'.format(youtube_url))
@@ -13,14 +14,17 @@ def youtube2mp3(youtube_url, line_userid):
     # 動画の情報を出力
     for lis in yt.streams.all():
         logger.info(lis)
-
     # get_bu_itagtodownloadメソッドででダウンロードができる
     yt2mp3 = yt.streams.get_by_itag(140).download('tmp/')
     print(yt2mp3)
     logger.info('youtube video converted to mp4 {}'.format(yt2mp3))
     # LINEのuseridにrenameする
-    input_file_path = 'tmp/{}.mp4'.format(line_userid)
+    input_file_path = '/tmp/{}.mp4'.format(line_userid)
     os.rename(yt2mp3, input_file_path)
+    # mp3ファイルを5分にカットする
+    mp4 = AudioSegment.from_file(input_file_path, format='mp4')
+    # 0~500sec(300000ms)にカット
+    mp4_1min = mp4[0:60000]
     # logger.info('rename: {}'.format(os.rename(yt2mp3, 'tmp/{}.mp4'.format(line_userid))))
     logger.info('Receive mp4 file name: {}'.format(input_file_path))
     # with open(yt2mp3, 'wb') as fb:
